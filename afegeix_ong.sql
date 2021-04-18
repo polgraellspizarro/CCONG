2908f16a45064f122e4ba6924bbb88d19fc9a927
root <root@inf1-pogrpi.fjeclot.net>
2021-04-18 18:43:59 +0200

    Segon commit amb el tema de ong

---
+++ afegeix_ong.sql
@@ -0,0 +1,3 @@
+alter table CCONG.ongs modify created_at timestamp not null;
+alter table CCONG.ongs modify updated_at timestamp not t null;
+insert into CCONG.ongs (cif,nom,adreca,poblacio,comarca,tipus) values ("456", "ong1", "c valencia 01", "Barcelona", "Barcelona", "Diners per a Africa");
